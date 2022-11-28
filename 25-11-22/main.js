434a0f5e5d98852a8b574d5dd9f91793ce49d45b
Martina-LP <madlp8@gmail.com>
2022-11-28 14:40:20 +0100

    es 25-11-22 edited with all ops

--- 25-11-22/main.js
+++ 25-11-22/main.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const bodyparser = require('body-parser')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/src'));
 
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html")
 });
 
@@ -12,6 +13,39 @@ app.post('/', (req, res) => {
   const n1 = Number(req.body.num1)
   const n2 = Number(req.body.num2)
   const calculate = n1 + n2
+  res.send('The result is: ' + calculate)
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+});
+
+app.post('/sub', (req, res) => {
+  const n1 = Number(req.body.num1)
+  const n2 = Number(req.body.num2)
+  const calculate = n1 - n2
+  res.send('The result is: ' + calculate)
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+});
+
+app.post('/mul', (req, res) => {
+  const n1 = Number(req.body.num1)
+  const n2 = Number(req.body.num2)
+  const calculate = n1 * n2
+  res.send('The result is: ' + calculate)
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+});
+
+app.post('/div', (req, res) => {
+  const n1 = Number(req.body.num1)
+  const n2 = Number(req.body.num2)
+  const calculate = n1 / n2
   res.send('The result is: ' + calculate)
 });
 
