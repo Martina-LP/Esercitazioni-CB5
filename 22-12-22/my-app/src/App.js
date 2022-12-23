6b5c84248573c8222c8e9d45e8ca422f36273ad5
Martina-LP <madlp8@gmail.com>
2022-12-23 01:15:01 +0100

    terzo esercizio con React edited

--- 22-12-22/my-app/src/App.js
+++ 22-12-22/my-app/src/App.js
@@ -3,6 +3,7 @@ import {Button} from './atoms/button/Button';
 import { Text } from './atoms/text/Text';
 import { Header } from './components/header/Header';
 import { Footer } from './components/footer/Footer';
+import { Container } from './layout/container/Container';
 
 function App() {
 
@@ -13,51 +14,57 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <Text 
-        as = 'h1'
-        style = {{ color: customColorOne }} 
-        variant = 'title'
-        >
-        The way to get started...
-      </Text>
-      <Text 
-        as = 'h2' 
-        style = {{ color: customColorTwo }}
-        variant = 'subtitle'
-        >
-        is to quit talking and begin doing.
-      </Text>
-      <Text 
-        as = 'h3' 
-        style = {{ color: customColorThree }}
-        variant = 'signature'
-        >
-        - Walt Disney
-      </Text>
-      <Button 
+      <main>
+        <Container withRow={0}>
+          <Text 
+          as = 'h1'
+          style = {{ color: customColorOne }} 
+          variant = 'title'
+          >
+          The way to get started...
+          </Text>
+          <Text 
+            as = 'h2' 
+            style = {{ color: customColorTwo }}
+            variant = 'subtitle'
+            >
+            is to quit talking and begin doing.
+          </Text>
+          <Text 
+            as = 'h3' 
+            style = {{ color: customColorThree }}
+            variant = 'signature'
+            >
+            - Walt Disney
+          </Text>
+        </Container>
+        
+        <Button 
         aria-label = 'Primo bottone' 
         variant = 'primary'
         onClick = {() => console.log('You clicked on the first button :)')}
         >
         Ready,
-      </Button>
-      <Button 
-        aria-label = 'Secondo bottone'
-        variant = 'secondary' 
-        onClick = {() => console.log('You clicked on the second button :)')}
-        >
-        set...
-      </Button>
-      <Button
-        aria-label = 'Terzo bottone'
-        variant = 'tertiary'
-        onClick = {() => console.log('You clicked on the third button :)')}
-        >
-        Go!
-      </Button>
+        </Button>
+        <Button 
+          aria-label = 'Secondo bottone'
+          variant = 'secondary' 
+          onClick = {() => console.log('You clicked on the second button :)')}
+          >
+          set...
+        </Button>
+        <Button
+          aria-label = 'Terzo bottone'
+          variant = 'tertiary'
+          onClick = {() => console.log('You clicked on the third button :)')}
+          >
+          Go!
+        </Button>
+      </main>
+      
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
